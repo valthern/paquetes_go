d55667bb09b59ac0bcd1106ddccafa28aff92971
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-06-04 23:49:44 -0600

    edteam -> importar paquetes de terceros completada

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/valthern/paquetes_go/greet"
 	"fmt"
+	"rsc.io/quote"
 )
 
 func main()  {
 	fmt.Println(greet.Italian())
 	fmt.Println(greet.Russian())
+	fmt.Println(quote.Hello())
 }

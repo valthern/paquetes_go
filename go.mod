2c709c8a761d30ccbb63976cde6e2c654b0b9820
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-07-06 08:45:14 -0600

    obtencion de version mayor con "go get [nombrePaquete]"

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require rsc.io/quote v1.5.2
 
 require (
 	golang.org/x/text v0.9.0 // indirect
+	rsc.io/quote/v3 v3.1.0
 	rsc.io/sampler v1.3.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"github.com/valthern/paquetes_go/greet"
 	"fmt"
 	"rsc.io/quote"
+	quotev3 "rsc.io/quote/v3"
 )
 
 func main()  {
 	fmt.Println(greet.Italian())
 	fmt.Println(greet.Russian())
 	fmt.Println(quote.Hello())
+	fmt.Println(quotev3.Concurrency())
+	fmt.Println(quotev3.HelloV3())
 }
